5df4f86fad866a985a963122bb490861cc9abbec
kin <kin@kins-MacBook-Air.local>
2025-12-14 10:49:08 -0500

    (nvim) configure biome for (j|t)sx? & jsonc?

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "biome" },
+      javascriptreact = { "biome" },
+      typescript = { "biome" },
+      typescriptreact = { "biome" },
+      json = { "biome" },
+      jsonc = { "biome" },
+    },
+  },
+}
