55061cba86166d762cf913d795ca9800729b7a3e
kin <kin@kins-MacBook-Air.local>
2025-12-14 22:23:48 -0500

    rm lazy

--- nvim/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- Configure Python provider to use project virtual environment
-vim.g.python3_host_prog = "~/.config/nvim/.venv/bin/python"
