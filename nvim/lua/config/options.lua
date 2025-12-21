b5780e2b5bc3f9b6117ba9dfec5b4bf1e1352ac3
kin <kin@kins-MacBook-Air.local>
2025-12-21 09:30:13 -0500

    (nvim) use ty as py lsp

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Configure Python provider to use project virtual environment
+-- Configure Neovim's Python virtual environment
 vim.g.python3_host_prog = "~/.config/nvim/.venv/bin/python"
+
+-- Configure Python LSP
+vim.g.lazyvim_python_lsp = "ty"
