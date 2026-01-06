119bc34f0f57b73f89ca4a93b1f8821cbf501e78
Kin <kin.matthews@mews.com>
2026-01-06 11:38:04 +0100

    (zellij) ref func to rnm tab to dir

--- fish/functions/zellij_tab_name.fish
+++ fish/functions/zellij_tab_name.fish
@@ -1,28 +1,32 @@
 # Auto-rename zellij tabs based on running command
 # Renames tab to command while running, resets to directory name when done
 
+function __zellij_rename_tab_to_dir
+    zellij action rename-tab (basename $PWD)
+end
+
 function zellij_tab_name_update_preexec --on-event fish_preexec
     if set -q ZELLIJ
         # Get just the command name (first word)
         set -l cmd (string split ' ' -- $argv[1])[1]
-        zellij action rename-tab $cmd
+        __zellij_rename_tab_to_dir
     end
 end
 
 function zellij_tab_name_update_postexec --on-event fish_postexec
     if set -q ZELLIJ
-        zellij action rename-tab (basename $PWD)
+        __zellij_rename_tab_to_dir
     end
 end
 
 function zellij_tab_name_update_pwd --on-variable PWD
     if set -q ZELLIJ
         # Only update if no command is actively running
-        zellij action rename-tab (basename $PWD)
+        __zellij_rename_tab_to_dir
     end
 end
 
 # Set initial tab name on shell startup
 if set -q ZELLIJ
-    zellij action rename-tab (basename $PWD)
+    __zellij_rename_tab_to_dir
 end
